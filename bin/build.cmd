2c6b0456b8a1a06b6978ffd1f7f70be73e6c715f
Jason Belt <belt@ksu.edu>
2022-11-18 15:47:34 -0600

    install tools via kekinian

--- bin/build.cmd
+++ bin/build.cmd
@@ -69,182 +69,6 @@ val project: Os.Path = homeBin / "project4testing.cmd"
 var didTipe = F
 var didCompile = F
 
-val versions = (home / "versions.properties").properties
-
-val cache: Os.Path = Os.env("SIREUM_CACHE") match {
-  case Some(p) =>
-    val d = Os.path(p)
-    if (!d.exists) {
-      d.mkdirAll()
-    }
-    d
-  case _ => Os.home / "Downloads" / "sireum"
-}
-
-def installCoursier(): Unit = {
-  val version = versions.get("org.sireum.version.coursier").get
-  val ver = home / "lib" / "coursier.jar.ver"
-  if (ver.exists && ver.read == version) {
-    return
-  }
-
-  val drop = cache / s"coursier-$version.jar"
-  if (!drop.exists) {
-    println(s"Downloading Coursier $version ...")
-    val url = s"https://github.com/coursier/coursier/releases/download/v$version/coursier.jar"
-    drop.downloadFrom(url)
-    println()
-  }
-
-  val coursierJar = home / "lib" / "coursier.jar"
-  drop.copyOverTo(coursierJar)
-
-  ver.writeOver(version)
-}
-
-def platformKind(kind: Os.Kind.Type): String = {
-  kind match {
-    case Os.Kind.Win => return "win"
-    case Os.Kind.Linux => return "linux"
-    case Os.Kind.LinuxArm => return "linux/arm"
-    case Os.Kind.Mac => return "mac"
-    case _ => return "unsupported"
-  }
-}
-
-def installZ3(kind: Os.Kind.Type): Unit = {
-  val version = versions.get("org.sireum.version.z3").get
-  val dir = homeBin / platformKind(kind) / "z3"
-  val ver = dir / "VER"
-
-  if (ver.exists && ver.read == version) {
-    return
-  }
-
-  val filename: String = kind match {
-    case Os.Kind.Win => s"z3-$version-x64-win.zip"
-    case Os.Kind.Linux => s"z3-$version-x64-glibc-2.31.zip"
-    case Os.Kind.Mac =>
-      if (ops.StringOps(proc"uname -m".run().out).trim == "arm64") s"z3-$version-arm64-osx-11.0.zip"
-      else s"z3-$version-x64-osx-10.16.zip"
-    case _ => return
-  }
-
-  val bundle = cache / filename
-
-  if (!bundle.exists) {
-    println(s"Please wait while downloading Z3 $version ...")
-    bundle.up.mkdirAll()
-    bundle.downloadFrom(s"https://github.com/Z3Prover/z3/releases/download/z3-$version/$filename")
-    println()
-  }
-
-  println("Extracting Z3 ...")
-  bundle.unzipTo(dir.up)
-
-  for (p <- dir.up.list if ops.StringOps(p.name).startsWith("z3-")) {
-    dir.removeAll()
-    p.moveTo(dir)
-  }
-
-  kind match {
-    case Os.Kind.Linux => (dir / "bin" / "z3").chmod("+x")
-    case Os.Kind.Mac => (dir / "bin" / "z3").chmod("+x")
-    case _ =>
-  }
-
-  ver.writeOver(version)
-}
-
-def installCVC(kind: Os.Kind.Type): Unit = {
-  def installCVCGen(gen: String, version: String): Unit = {
-    val genOpt: Option[String] = if (gen == "4") None() else Some(gen)
-    val exe = homeBin / platformKind(kind) / (if (kind == Os.Kind.Win) st"cvc$genOpt.exe" else st"cvc$genOpt").render
-    val ver = homeBin / platformKind(kind) / st".cvc$genOpt.ver".render
-
-    val VER = s"$gen-$version"
-
-    if (ver.exists && ver.read == VER) {
-      return
-    }
-
-    val (sub, filename, dropname): (String, String, String) = (gen, kind) match {
-      case (string"5", Os.Kind.Win) => (s"cvc$gen-$version", s"cvc$gen-Win64.exe", s"cvc$gen-$version-Win64.exe")
-      case (string"5", Os.Kind.Linux) => (s"cvc$gen-$version", s"cvc$gen-Linux", s"cvc$gen-$version-Linux")
-      case (string"5", Os.Kind.Mac) =>
-        if (ops.StringOps(proc"uname -m".run().out).trim == "arm64")
-          (s"cvc$gen-$version", s"cvc$gen-macOS-arm64", s"cvc$gen-$version-macOS-arm64")
-        else (s"cvc$gen-$version", s"cvc$gen-macOS", s"cvc$gen-$version-macOS")
-      case (string"4", Os.Kind.Win) => (version, s"cvc$gen-$version-win64-opt.exe", s"cvc$gen-$version-win64-opt.exe")
-      case (string"4", Os.Kind.Linux) => (version, s"cvc$gen-$version-x86_64-linux-opt", s"cvc$gen-$version-x86_64-linux-opt")
-      case (string"4", Os.Kind.Mac) => (version, s"cvc$gen-$version-macos-opt", s"cvc$gen-$version-macos-opt")
-      case _ => return
-    }
-
-    val drop = cache / dropname
-
-    if (!drop.exists) {
-      println(s"Please wait while downloading CVC$gen $version ...")
-      drop.up.mkdirAll()
-      drop.downloadFrom(s"https://github.com/cvc5/cvc5/releases/download/$sub/$filename")
-      println()
-    }
-
-    drop.copyOverTo(exe)
-
-    kind match {
-      case Os.Kind.Linux => exe.chmod("+x")
-      case Os.Kind.Mac => exe.chmod("+x")
-      case _ =>
-    }
-
-    ver.writeOver(VER)
-    println()
-  }
-  val (gen1, genVersion1, gen2, genVersion2): (String, String, String, String) =
-    ops.StringOps(versions.get("org.sireum.version.cvc").get).split((c: C) => c == '-' || c == ',') match {
-      case ISZ(g1, gv1, g2, gv2) => (g1, gv1, g2, gv2)
-      case ISZ(string"1.8") => ("4", "1.8", "4", "1.8")
-      case ISZ(version) => ("5", version, "5", version)
-    }
-  installCVCGen(gen1, genVersion1)
-  if (gen1 != gen2) {
-    installCVCGen(gen2, genVersion2)
-  }
-}
-
-
-def installAltErgoOpen(kind: Os.Kind.Type): Unit = {
-  val version = versions.get("org.sireum.version.alt-ergo-open").get
-  val dir = homeBin / platformKind(kind)
-  val exe = dir / "alt-ergo-open"
-  val ver = dir / ".alt-ergo-open.ver"
-
-  if (ver.exists && ver.read == version) {
-    return
-  }
-
-  val filename: String = kind match {
-    case Os.Kind.Linux => s"alt-ergo-open-$version-linux"
-    case Os.Kind.Mac => s"alt-ergo-open-$version-mac"
-    case _ => return
-  }
-
-  val drop = cache / filename
-
-  if (!drop.exists) {
-    println(s"Please wait while downloading Alt-Ergo $version (Apache 2.0 Licence) ...")
-    drop.up.mkdirAll()
-    drop.downloadFrom(s"https://github.com/sireum/rolling/releases/download/alt-ergo-open/$filename")
-    println()
-  }
-
-  drop.copyOverTo(exe)
-
-  exe.chmod("+x")
-
-  ver.writeOver(version)
-}
 
 def clone(repo: String): Unit = {
   if (!(home / repo).exists) {
@@ -285,10 +109,21 @@ for (m <- ISZ("runtime", "slang", "logika")) {
   clone(m)
 }
 
-installZ3(Os.kind)
-installCVC(Os.kind)
-installAltErgoOpen(Os.kind)
-installCoursier()
+def installToolsViaKekinian(): Unit = {
+  val builtIn = home / "runtime" / "library" / "shared" / "src" / "main" / "scala" / "org" / "sireum" / "BuiltInTypes.slang"
+  if(!builtIn.exists) {
+    builtIn.write(".")
+  }
+  val kbuild = homeBin / "kbuild.cmd"
+  kbuild.downloadFrom("https://raw.githubusercontent.com/sireum/kekinian/master/bin/build.cmd")
+  proc"$sireum slang run $kbuild --help".at(homeBin).runCheck()
+  kbuild.remove()
+  if(builtIn.size == 1) {
+    (home / "runtime").removeAll()
+  }
+}
+
+installToolsViaKekinian()
 
 for (i <- 0 until Os.cliArgs.size) {
   Os.cliArgs(i) match {
